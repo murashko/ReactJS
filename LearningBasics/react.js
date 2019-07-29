eb2f186bd71f100c8f3703ffcb67d5c4fd2f2b1e
murashko <serhy.zastavny@gamil.com>
2019-07-29 09:14:09 +0300

    Lesson 2: Connected React

---
+++ LearningBasics/react.js
@@ -0,0 +1,27 @@
+function Card(props) {
+    return (
+        <div className="card">
+            <div className="card-img">
+                <img
+                    src={props.car.img}
+                    alt={props.car.name} />
+            </div>
+            <h3>{props.car.name}</h3>
+            <p>{props.car.price} $</p>
+        </div>
+    )
+}
+
+const app = (
+    <div className ="app">
+        <div className ="list">
+            <Card car = {{name: "BMW M2 Coupe", price: 20000, img: "https://mochamanstyle.com/wp-content/uploads/2015/10/2016-BMW-M2-Coupe.jpg"}}/>
+            <Card car = {{name: "Audi TT", price: 15000, img: "https://article.images.consumerreports.org/w_598,h_436/prod/content/dam/cro/news_articles/cars/2016-Audi-TT-pr-1-2016-598"}}/>
+            <Card car = {{name: "Rolls Royce", price: 50000, img: "http://a2goos.com/data_images/models/rolls-royce-phantom-coupe/rolls-royce-phantom-coupe-03.jpg"}}/>
+            <Card car = {{name: "Mercedes Amg Coupe", price: 18000, img: "https://auto.ndtvimg.com/car-images/big/mercedes-amg/gle-coupe/mercedes-amg-gle-coupe.jpg?v=2"}}/>
+        </div>
+    </div>
+)
+
+ReactDOM.render(app,
+    document.getElementById('root'));
