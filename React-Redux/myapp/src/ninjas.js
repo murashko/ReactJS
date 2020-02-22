200d37ed8ef04193aaf8a45a14e68027b0ccac53
murashko <serhy.zastavny@gamil.com>
2020-02-22 10:51:32 +0200

    lesson 15

--- React-Redux/myapp/src/ninjas.js
+++ React-Redux/myapp/src/ninjas.js
@@ -1,25 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Ninjas extends Component {
-	render() {
-		const { ninjas } = this.props;
-		const ninjaList = ninjas.map(ninja => {
-			const { name, age, belt, id } = ninja;
-			return (
-				<div className="ninja" key={ id }>
-					<div>Name: { name }</div>
-					<div>Age: { age }</div>
-					<div>Belt: { belt }</div>
-				</div>
-			);
-		});
-
+const Ninjas = ({ninjas}) => {
+	const ninjaList = ninjas.map(ninja => {
+		const { name, age, belt, id } = ninja;
 		return (
-			<div className="ninja-list">
-				{ ninjaList }
+			<div className="ninja" key={ id }>
+				<div>Name: { name }</div>
+				<div>Age: { age }</div>
+				<div>Belt: { belt }</div>
 			</div>
 		);
-	}
-}
+	});
+
+	return (
+		<div className="ninja-list">
+			{ ninjaList }
+		</div>
+	);
+};
 
 export default Ninjas;
