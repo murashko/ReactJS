81508df8cd348cdab9c47931644e2c15f005a673
murashko <serhy.zastavny@gamil.com>
2020-02-22 13:10:00 +0200

    lesson 18

--- React-Redux/myapp/src/App.js
+++ React-Redux/myapp/src/App.js
@@ -10,13 +10,22 @@ class App extends React.Component {
 			{ name: 'Crystal', age: 25, belt: 'pink', id: 3 }
 		]
 	}
+
+	addNinja = (ninja) => {
+		ninja.id = Math.random();
+		const ninjas = [...this.state.ninjas, ninja];
+		this.setState({
+			ninjas: ninjas
+		});
+	}
+
 	render() {
 		return (
 			<div className="App">
 				<h1>My React app!</h1>
 				<p>Wellcome :)</p>
 				<Ninjas ninjas={ this.state.ninjas } />
-				<AddNinja />
+				<AddNinja addNinja={ this.addNinja }/>
 			</div>
 		);
 	}
