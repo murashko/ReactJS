b8d9555beedf572afd270a42c580700fe8da1c25
murashko <serhy.zastavny@gamil.com>
2020-02-22 10:00:42 +0200

    lesson 12

--- React-Redux/myapp/src/App.js
+++ React-Redux/myapp/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Ninjas from './ninjas';
 
 function App() {
   return (
     <div className="App">
       <h1>My React app!</h1>
 	  <p>Wellcome :)</p>
+	  <Ninjas />
     </div>
   );
 }
