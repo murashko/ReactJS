76ab324c1f5bdec42a57c3a4cec5998143f7fe3f
murashko <serhy.zastavny@gamil.com>
2020-02-25 09:25:10 +0200

    lesson 24

--- React-Redux/todoapp/src/App.js
+++ React-Redux/todoapp/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Todos from "./Todos";
+import AddTodo from "./AddTodo";
 
 class App extends Component {
 	state = {
@@ -7,6 +8,15 @@ class App extends Component {
 			{ id: 1, content: 'buy some milk' },
 			{ id: 2, content: 'play mario kart' }
 		]
+	}
+
+	addTodo = (todo) => {
+		const {todos} = this.state;
+		todo.id = todos.reduce((first, second) => Math.max(first.id, second.id)) + 1;
+		const newTodos = [...todos, todo];
+		this.setState({
+			todos: newTodos
+		});
 	}
 
 	deleteTodo = (id) => {
@@ -21,6 +31,7 @@ class App extends Component {
 			<div className="todo-app container">
 				<h1 className="center blue-text">Todo's</h1>
 				<Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+				<AddTodo addTodo={this.addTodo} />
 			</div>
 		);
 	}
