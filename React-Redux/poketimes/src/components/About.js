ce28831b4eff18352ff4eb3ed6fdf4d1acd31cc6
murashko <S.Zastavnyi@salesup-it.local>
2020-02-25 19:35:41 +0200

    lesson 25

---
+++ React-Redux/poketimes/src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+	return (
+		<div className="container">
+			<h4 className="center">About</h4>
+			<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis, ea tempora temporibus et autem recusandae eveniet possimus beatae modi. Maxime animi dolores debitis. Corrupti aperiam a sit magnam dolor illo?</p>
+		</div>
+	);
+}
+
+export default About;
